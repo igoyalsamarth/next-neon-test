a4430ef8f219c686c3520020e15f764ee6750fc6
Samarth Goyal <samarth.goyal1999@gmail.com>
2023-09-28 19:17:34 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first")
 const nextConfig = {
     images: {
         domains: ['res.cloudinary.com']
       },
     experimental: {
       esmExternals: false,
-      }
+      },
+      
 }
 
 module.exports = nextConfig
