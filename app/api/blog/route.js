9cd3c4f8b3382d4f5ebd00d0b31a04fed57e265a
Samarth Goyal <samarth.goyal1999@gmail.com>
2023-09-29 13:01:39 +0530

    dynamics

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import db from '@/lib/db'
  
-export const revalidate = 1;
+export const dynamic = 'force-dynamic';
 
 export async function GET(request) {
   let tags;
